322c52dc3f92dbb3ad0fb885e080b1285b8744bf
sitra <sitramohammed75@gmail.com>
2024-05-21 06:11:57 +0300

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route,Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import ShowBooks from './pages/ShowBooks'
-import CreateBook from './pages/CreateBook'
-import UpdateBook from './pages/UpdateBook'
-import DeleteBook from './pages/DeleteBook'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import ShowBooks from './pages/ShowBooks';
+import CreateBook from './pages/CreateBook';
+import UpdateBook from './pages/UpdateBook';
+import DeleteBook from './pages/DeleteBook';
 
 function App() {
- 
   return (
     <>
-    <Routes>
-      <Route path='/' element={Home}></Route>
-      <Route path='/books/detail/:id' element={ShowBooks}></Route>
-      <Route path='/books/create' element={CreateBook}></Route>
-      <Route path='/books/edit/:id' element={UpdateBook}></Route>
-      <Route path='/books/delete/:id' element={DeleteBook}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/books/detail/:id" element={<ShowBooks />}></Route>
+        <Route path="/books/create" element={<CreateBook />}></Route>
+        <Route path="/books/edit/:id" element={<UpdateBook />}></Route>
+        <Route path="/books/delete/:id" element={<DeleteBook />}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import Spinner from '../components/Spinner';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [books, setBooks] = useState([]);
+  const [loading, setLoad] = useState(false);
 
-export default Home
+  useEffect(() => {
+    setLoad(true);
+    axios
+      .get('http://localhost:5555/books')
+      .then((response) => {
+        setBooks(response.data);
+        setLoad(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoad(false);
+      });
+  }, []);
+
+  return (
+    <div className="p-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl my-8">Book List</h1>
+        <Link to="/books/create">
+          <MdOutlineAddBox className="text-sky-800 text-4xl" />
+        </Link>
+      </div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <table className="border-separate border-spacing-2 w-full">
+          <thead>
+            <tr>
+              <th className="border border-gray-500 rounded-md">no</th>
+              <th className="border border-gray-500 rounded-md">title</th>
+              <th className="border border-gray-500 rounded-md max-md:hidden">author</th>
+              <th className="border border-gray-500 rounded-md max-md:hidden">publish year</th>
+              <th className="border border-gray-500 rounded-md">operation</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book, index) => (
+              <tr key={book._id} className="h-8">
+                <td className="border border-slate-700 rounded-md text-center">{index + 1}</td>
+                <td className="border border-slate-700 rounded-md text-center">{book.title}</td>
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden">{book.author}</td>
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden">{book.publicationYear}</td>
+                <td className="border border-slate-700 rounded-md text-center">
+                  <div className="flex justify-center gap-x-4">
+                    <Link to={`/books/detail/${book._id}`}>
+                      <BsInfoCircle className="text-2xl text-green-800" />
+                    </Link>
+                    <Link to={`/books/edit/${book._id}`}>
+                      <AiOutlineEdit className="text-2xl text-yellow-600" />
+                    </Link>
+                    <Link to={`/books/delete/${book._id}`}>
+                      <MdOutlineDelete className="text-2xl text-red-600" />
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default Home;
